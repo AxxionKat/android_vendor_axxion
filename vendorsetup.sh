8be4d17617798e7672c87a44c93300c5e0a7d425
Sira Cuervo <fl.siracusa@outlook.com.ar>
2014-05-04 23:28:42 -0300

    Add i9505 to the lunch combo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo axxion_gee-userdebug
+add_lunch_combo axxion_jflte-userdebug
 add_lunch_combo axxion_maguro-userdebug
 add_lunch_combo axxion_mako-userdebug
 add_lunch_combo axxion_spyder-userdebug
