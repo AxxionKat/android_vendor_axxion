524a051fd08df0041cc1216d2a6de77383b816fc
Sira Cuervo <fl.siracusa@outlook.com.ar>
2014-05-10 20:31:17 -0300

    Add i9300 and i9500

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,7 @@
 add_lunch_combo axxion_d2lte-userdebug
 add_lunch_combo axxion_gee-userdebug
+add_lunch_combo axxion_i9300-userdebug
+add_lunch_combo axxion_i9500-userdebug
 add_lunch_combo axxion_jflte-userdebug
 add_lunch_combo axxion_maguro-userdebug
 add_lunch_combo axxion_mako-userdebug
