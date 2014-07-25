f11292e86c2dba17fa1824671262c0bb04fcd186
Sira Cuervo <fl.siracusa@outlook.com.ar>
2014-07-25 12:37:46 -0300

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -81,7 +81,7 @@ endif
 
 # Signature compatibility validation
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/bin/otasigcheck.sh:system/bin/otasigcheck.sh
+    vendor/axxion/prebuilt/common/bin/otasigcheck.sh:system/bin/otasigcheck.sh
 
 # init.d support
 PRODUCT_COPY_FILES += \
