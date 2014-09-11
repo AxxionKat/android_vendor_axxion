cda14879fdcb4a3cd566056a0e54fbe2a061ad30
merkrehan <mrkrehanishaq1990@gmail.com>
2014-09-11 23:50:36 +0530

    Tinted Status Bar [1/2]
    Credits Thinking Bridge Rom
    
    Change-Id: I49d831f802bd2c36b08d9384ce4d504e97ef1aa2
    
    Conflicts:
    	config/common.mk
    	config/packages.mk

--- config/common.mk
+++ config/common.mk
@@ -105,7 +105,10 @@ PRODUCT_COPY_FILES +=  \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-# Enable wireless Xbox 360 controller support
+#ChameleonEngine
+PRODUCT_COPY_FILES += \
+    vendor/axxion/proprietary/ChameleonEngine/ChameleonEngine.apk:system/app/ChameleonEngine.apk
+
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
 
