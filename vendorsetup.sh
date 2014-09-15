947789e23a6623d0fafebe76bd69aedf2c759bef
Sira Cuervo <fl.siracusa@outlook.com.ar>
2014-09-15 20:53:24 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@ add_lunch_combo axxion_d2lte-userdebug
 add_lunch_combo axxion_gee-userdebug
 add_lunch_combo axxion_hammerhead-userdebug
 add_lunch_combo axxion_hlte-userdebug
+add_lunch_combo axxion_hltespr-userdebug
 add_lunch_combo axxion_i9300-userdebug
 add_lunch_combo axxion_i9500-userdebug
 add_lunch_combo axxion_jflte-userdebug
